147d55f8e94f2a1613fc62937d2ea8d8604bd994
odol0503 <odol0503@users.noreply.github.com>
2017-02-28 09:18:03 +0900

    Update build_libx264_android.sh

--- build_libx264_android.sh
+++ build_libx264_android.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
 NDK=/home/shoh/Android/Sdk/ndk-bundle
-PLATFORM=$NDK/platforms/android-9/arch-arm/
-PREBUILT=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt
+PLATFORM=$NDK/platforms/android-21/arch-arm64/
+PREBUILT=$NDK/toolchains/aarch64-linux-android-4.9/prebuilt
 PREFIX=$NDK/sources/ffmpeg-3.2.4/libx264
 
-./configure --cross-prefix=$PREBUILT/linux-x86_64/bin/arm-linux-androideabi- \
---sysroot=$PLATFORM \
---host=arm-linux \
---enable-pic \
---enable-static \
---extra-cflags="-fPIE -pie" \
---extra-ldflags="-fPIE -pie" \
---disable-cli \
---disable-asm \
---prefix=$PREFIX \
---bin-dir=$PREFIX/bin
+function build_one
+{
+  ./configure \
+    --cross-prefix=$PREBUILT/linux-x86_64/bin/aarch64-linux-android- \
+    --sysroot=$PLATFORM \
+    --host=aarch64-linux \
+    --enable-pic \
+    --enable-static \
+    --extra-cflags="-fPIE -pie" \
+    --extra-ldflags="-fPIE -pie" \
+    --disable-cli \
+    --prefix=$PREFIX \
+    --bindir=$PREFIX/bin
+	make clean
+	make
+	make install
+}
+build_one
